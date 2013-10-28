22d4ed74e0a65be7a0c36d891065e7abe36ceb0d
BromleyJohn <john@fleetfacts.net>
2013-10-28 19:09:45 +0000

    init

--- www/js/index.js
+++ www/js/index.js
@@ -20,6 +20,24 @@ var app = {
     // Application Constructor
     initialize: function() {
         alert("Here!!!");
+
+         $.ajax({ // ajax call starts
+        
+         url: 'http://johnah.byethost7.com/cities.php?callback=?', // JQuery loads serverside.php
+        
+        //url: 'cities.php', // JQuery loads serverside.php
+          dataType: 'jsonp', // Choosing a JSON datatype json for local jasonp for crossweb - NOT SURE ABOUT THIS!!!
+          success: function(data) // Variable data contains the data we get from serverside
+          {
+          $('#cities').html(''); // Clear #bands div
+          for (var i in data) {
+             $('#cities').append(data[i] + '<br/>');
+          }
+
+        }
+    });
+
+
         this.bindEvents();
     },
     // Bind Event Listeners
